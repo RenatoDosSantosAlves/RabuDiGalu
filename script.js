8a10c5dd5c97a9bbdc7296c146bd66bcd89b3076
RenatoAlves <renatosakasaki34@gmail.com>
2025-07-25 20:38:33 -0300

    Ajustes para cel

--- script.js
+++ script.js
@@ -474,3 +474,38 @@ AOS.init({
     once: true,
     offset: 100
 });
+
+// Initialize Swiper for Instagram Feed
+document.addEventListener('DOMContentLoaded', function() {
+    new Swiper('.instagram-swiper', {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+            },
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 50,
+            },
+        },
+    });
+});
